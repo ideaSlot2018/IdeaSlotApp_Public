42f097a9c3fdec05e0fb4fa68830e2e69d042394
yuta-akazawa <grief137@gmail.com>
2019-04-07 03:04:00 +0900

    added MessageView in Idea register form

--- IdeaSlotApp/Classes/Extention/UIView.swift
+++ IdeaSlotApp/Classes/Extention/UIView.swift
@@ -24,3 +24,32 @@ extension UIView {
     }
 
 }
+
+protocol Nibable: NSObjectProtocol {
+    static var nibName: String { get }
+    static func nib() -> UINib
+}
+
+extension Nibable where Self: UIView {
+    static var nibName: String {
+        return className
+    }
+    
+    static func nib() -> UINib {
+        return UINib(nibName: nibName, bundle: nil)
+    }
+    
+    static func view() -> Self {
+        return nib().instantiate(withOwner: nil, options: nil).first as! Self
+    }
+}
+
+extension NSObjectProtocol {
+    static var className: String {
+        return String(describing: self)
+    }
+    
+    var className: String {
+        return type(of: self).className
+    }
+}
